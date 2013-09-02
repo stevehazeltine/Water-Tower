f24c49365f1640fe8c9c614cce34b84aba019121
josh.rathke <josh.rathke@ywammontana.org>
2013-09-01 20:54:21 -0700

    Minor fixes to sidebar, and a couple spelling corrections

---
+++ archive-projects.php
@@ -0,0 +1,34 @@
+<?php get_header() ?>
+
+	<div class="row archive-content-container">
+		<div class="col-md-8">
+			<?php get_active_project_index() ?>
+		</div><!--col-md-10-->
+		
+		
+		
+		
+		<div class="col-md-4 sidebar">
+			<ul>
+				<?php 
+				get_related_posts(array(
+					'title'=>'Project Updates',
+					'category_name'=>'project-updates',
+				));
+				
+				subscribe_widget();
+				?>
+			</ul>
+		</div><!--sidebar-container-->
+
+	</div><!--archive-content-container-->
+			
+		<div class="row">
+			<div class="col-md-9">
+				<?php if (function_exists("pagination")) {
+				    pagination($additional_loop->max_num_pages);
+				} ?>
+			</div>
+		</div>
+ 
+ <?php get_footer() ?>
