11636cfc9efd53524d1bf92c6f9d5526ea7390fb
stevehaz <stevehaz@vps11229.inmotionhosting.com>
2013-09-05 15:03:40 -0700

    Header Fix

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 		
     </head>
     <body data-spy="scroll" data-target="#scrollspy-nav">
-	Hello World
+	
 		<!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
